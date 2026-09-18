dcfa638ab4d7b5f7db7be346552b2a271a6a6560
Divya <2022wa86721@wilp.bits-pilani.ac.in>
2026-09-18 22:55:55 +0530

    Initial Commit

---
+++ src/main/java/com/bits/App.java
@@ -0,0 +1,11 @@
+package com.bits;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("DevOps Lab Assignment");
+        System.out.println("BITS ID: 2022wa86721");
+        System.out.println("Name: Divya");
+    }
+}
+
+
